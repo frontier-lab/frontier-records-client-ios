6cab574cb663de8b5cc3f025b7f7e3a390ef47d3
gwiyeong <gwiyeong@nhnent.com>
2019-06-18 18:53:36 +0900

    Initial Commit

---
+++ FrontierRecords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrontierRecords
+//
+//  Created by NHNEnt on 18/06/2019.
+//  Copyright © 2019 frontier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
